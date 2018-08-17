febd4b6bf2eea3ea7de8627f131747d3af425804
David Mullin <david_mullin@me.com>
2018-08-16 17:24:12 -0700

    added playMove function

--- projects/minesweeper/src/minesweeper.js
+++ projects/minesweeper/src/minesweeper.js
@@ -1,3 +1,25 @@
+// create class Game
+class Game {
+    constructor(numberOfRows, numberOfColumns, numberOfBombs) {
+        this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
+    }
+    playMove(rowIndex, columnIndex) {
+        this._board.flipTile(rowIndex, columnIndex);
+        if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
+            console.log('The Game is Over!');
+            this._board.print();
+        } else if (this._board.hasSafeTiles()) {
+            console.log('Congratulations! You Won the Game!');
+        } else {
+            console.log('Current Board:');
+            this._board.print();
+        }
+    }
+}
+
+
+
+
 // create class Board
 class Board {
     constructor(numberOfRows, numberOfColumns, numberOfBombs) {
