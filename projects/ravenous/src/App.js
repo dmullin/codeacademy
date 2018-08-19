c83ffc812f00a6f454c9e530d6790e51a354edd9
David Mullin <david_mullin@me.com>
2018-08-19 14:59:24 -0700

    modified app.js

--- projects/ravenous/src/App.js
+++ projects/ravenous/src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Hello Codeacademy!
-        </p>
-      </div>
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList /> 
+    </div>
     );
   }
 }
