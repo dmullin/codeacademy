a20b9808f1eed30487df07603fb61f8829d9030c
David Mullin <david_mullin@me.com>
2018-08-22 11:07:53 -0700

    created Yelp module

---
+++ projects/ravenous/src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey = 'vvCWGPFINllr9_x3FJdYI7ulpxfOJ92GeSSx74kjD0Zw4MZLmduSoLf3fGFWu9sU9jc-02Ec6AqC3hMtRNWGtdtH8OZGWHIlZ_N845WvlAkMXDFp81AKuIxeSpx9W3Yx';
+
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=&${location}&sort_by=S&${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.buisnesses) {
+                return jsonResponse.businesses.map(business => {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.address1,
+                    city: business.city,
+                    state: business.state,
+                    zipCode: business.zip_code,
+                    category: business.categories,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+    
+                });
+            }
+        });
+    }
+};
+
+export default Yelp;
