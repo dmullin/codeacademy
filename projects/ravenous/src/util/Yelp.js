5eed16d8d15c165666568ab20ae9df26639f6f47
David Mullin <david_mullin@me.com>
2018-08-22 13:31:50 -0700

    resolved syntax issues

--- projects/ravenous/src/util/Yelp.js
+++ projects/ravenous/src/util/Yelp.js
@@ -3,14 +3,14 @@ const apiKey = 'vvCWGPFINllr9_x3FJdYI7ulpxfOJ92GeSSx74kjD0Zw4MZLmduSoLf3fGFWu9sU
 
 const Yelp = {
     search(term, location, sortBy) {
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=&${location}&sort_by=S&${sortBy}`, {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
             headers: {
                 Authorization: `Bearer ${apiKey}`
             }
         }).then(response => {
             return response.json();
         }).then(jsonResponse => {
-            if (jsonResponse.buisnesses) {
+            if (jsonResponse.businesses) {
                 return jsonResponse.businesses.map(business => ({
                     id: business.id,
                     imageSrc: business.image_url,
@@ -18,7 +18,7 @@ const Yelp = {
                     address: business.location.address1,
                     city: business.location.city,
                     state: business.location.state,
-                    zipCode: business.locaton.zip_code,
+                    zipCode: business.location.zip_code,
                     category: business.categories[0].title,
                     rating: business.rating,
                     reviewCount: business.review_count
