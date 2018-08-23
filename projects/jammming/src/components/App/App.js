6ade29f19594daedc4c38bd4175528af870945fb
David Mullin <david_mullin@me.com>
2018-08-23 10:51:14 -0700

    inital steps for setup

---
+++ projects/jammming/src/components/App/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchResults: [{
+        name: name,
+        artist: artist,
+        album: album,
+        id: id
+      }]
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <!-- Add a SearchBar component -->
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <!-- Add a Playlist component -->
+          </div>
+        </div>
+    </div>
+    );
+  }
+}
+
+export default App;
