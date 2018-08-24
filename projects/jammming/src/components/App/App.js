12fbb2d51af8b4dffe32a59afee56a3297076921
David Mullin <david_mullin@me.com>
2018-08-23 17:24:43 -0700

    change the name of the playlist

--- projects/jammming/src/components/App/App.js
+++ projects/jammming/src/components/App/App.js
@@ -26,6 +26,7 @@ class App extends React.Component {
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   // method to add tracks to playlist
@@ -56,7 +57,11 @@ class App extends React.Component {
           <!-- Add a SearchBar component -->
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.state.addTrack()} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack()} />
+            <Playlist 
+              playlistName={this.state.playlistName} 
+              playlistTracks={this.state.playlistTracks} 
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName} />
           </div>
         </div>
     </div>
